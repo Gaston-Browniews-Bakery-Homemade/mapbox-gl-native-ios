17ad571693c93b000070fabf6d8292c78c0b5345
Minh Nguyễn <mxn@1ec5.org>
2020-04-06 09:40:10 -0700

    [ios, macos] Initialize offline storage on main thread before snapshotting

--- platform/darwin/src/MGLMapSnapshotter.mm
+++ platform/darwin/src/MGLMapSnapshotter.mm
@@ -301,7 +301,12 @@ private:
     if (!completion) {
         return;
     }
-    if (!mbgl::Scheduler::GetCurrent()) {
+    
+    // Ensure that offline storage has been initialized on the main thread, as MGLMapView and MGLOfflineStorage do when used directly.
+    // https://github.com/mapbox/mapbox-gl-native-ios/issues/227
+    if ([NSThread.currentThread isMainThread]) {
+        (void)[MGLOfflineStorage sharedOfflineStorage];
+    } else {
         [NSException raise:NSInvalidArgumentException
                     format:@"startWithQueue:completionHandler: must be called from a thread with an active run loop."];
     }
